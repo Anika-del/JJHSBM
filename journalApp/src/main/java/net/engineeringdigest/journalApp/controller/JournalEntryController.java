f4a5d0a8b4ad8ec89b3d720cd694b48e1a5dd162
Anika Patel <76812213+Anika-del@users.noreply.github.com>
2025-02-11 22:23:41 +0530

    journal-app

--- journalApp/src/main/java/net/engineeringdigest/journalApp/controller/JournalEntryController.java
+++ journalApp/src/main/java/net/engineeringdigest/journalApp/controller/JournalEntryController.java
@@ -2,9 +2,7 @@ package net.engineeringdigest.journalApp.controller;
 
 import net.engineeringdigest.journalApp.entity.JournalEntry;
 import org.apache.commons.collections.FastHashMap;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -17,9 +15,25 @@ public class JournalEntryController {
 
     private Map<Long, JournalEntry> journalEntries = new HashMap<>();
 
-    @GetMapping("/abc")
+    @GetMapping
     public List<JournalEntry> getAll(){
         return new ArrayList<>(journalEntries.values());
+        // //localhost:8081/journal    for Get body row json
+    }
+    @PostMapping
+    public boolean createEntry(@RequestBody JournalEntry myEntry){
+        //localhost:8081/journal    for Post body row json
+        journalEntries.put(myEntry.getId(), myEntry);
+        return true;
     }
 
-}
+    @PutMapping("/{id}")
+    public boolean updateEntry(@PathVariable Long id, @RequestBody JournalEntry updatedEntry) {
+        if (journalEntries.containsKey(id)) {
+            journalEntries.put(id, updatedEntry);
+            return true;
+        }
+        return false;
+
+
+    }}
