e79ee6122a4da8e39e83fdc1dabaa0cf8ef883ab
Anika Patel <76812213+Anika-del@users.noreply.github.com>
2025-02-12 22:37:28 +0530

    journal app

--- journalApp/src/main/java/net/engineeringdigest/journalApp/controller/JournalEntryControllerV2.java
+++ journalApp/src/main/java/net/engineeringdigest/journalApp/controller/JournalEntryControllerV2.java
@@ -10,42 +10,36 @@ import java.util.List;
 import java.util.Map;
 
 @RestController
-//@RequestMapping("/journal")
-@RequestMapping("_/journal")
-public class JournalEntryController {
+@RequestMapping("/journal")
+public class JournalEntryControllerV2 {
 
-    private Map<Long, JournalEntry> journalEntries = new HashMap<>();
-
-    //isse sabhi id title content aa jayega
     @GetMapping
     public List<JournalEntry> getAll() {
-        return new ArrayList<>(journalEntries.values());
-        // //localhost:8081/journal    for Get body row json
+        return null;
     }
 
-    //data send krne ke liye id title content
     @PostMapping
     public boolean createEntry(@RequestBody JournalEntry myEntry) {
-        //localhost:8081/journal    for Post body row json
-        journalEntries.put(myEntry.getId(), myEntry);
+
         return true;
     }
 
-    //one id title content dekhna ho to use this //spacfic general entry
     @GetMapping("id/{myId}")
     public JournalEntry getJournalEnteryById(@PathVariable long myId) {
-        return journalEntries.get(myId);
+        return null;
+
     }
 
     //
     @DeleteMapping("id/{myId}")
     public JournalEntry deleteJournalEnteryById(@PathVariable long myId) {
-        return journalEntries.remove(myId);
+        return null;
+
     }
 
     @PutMapping("id/{id}")
     public JournalEntry updateJournalById(@PathVariable Long id, @RequestBody JournalEntry myEntry) {
-        return journalEntries.put(id, myEntry);
+        return null;
     }
 
 
